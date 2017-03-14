550634bfd98ac8a5f7770cd25faa5c5552cabcda
marlonnn <zhongw_1988@126.com>
2017-03-14 15:12:35 +0800

    添加Splash Screen开始启动界面

---
+++ CiiTech.Demo/Demo.SplashScreen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo.SplashScreen
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            SplashScreenForm.ShowSplash();
+
+
+            SplashScreenForm.CloseSplash();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static string MajorVersion
+        {
+            get
+            {
+                return GetMajorVersion(Application.ProductVersion);
+            }
+        }
+
+        public static string GetMajorVersion(string version)
+        {
+            if (version != null && version.Length > 5) version = version.Substring(0, version.Length - 5);
+            return version;
+        }
+    }
+}
---
+++ CiiTech.Demo/Demo.SplashScreen/readme.txt
@@ -0,0 +1,1 @@
+﻿Splash Screen开始启动界面
