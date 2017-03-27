f3e55677b62e664406926ab6215ea37084483dc1
marlonnn <zhongw_1988@126.com>
2017-03-27 16:44:50 +0800

    添加pdf demo初始化代码

---
+++ CiiTech.Demo/Demo.Pdf/Reports/ReportPageNumberItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Pdf.Reports
+{
+    [Serializable]
+    public class ReportPageNumberItem : ReportItemBase
+    {
+        private int itemStyle;
+        /// <summary>
+        /// 1:1,2,3...;2: 1/3,2/3...
+        /// </summary>
+        public int ItemStyle
+        {
+            get { return itemStyle; }
+            set { itemStyle = value; }
+        }
+
+    }
+}
